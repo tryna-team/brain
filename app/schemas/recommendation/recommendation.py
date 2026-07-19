dc70f65456c8aaae52d220826dd2e6a0fdf5d46b
윤예림 <yereme@yun-yelim-ui-MacBookPro.local>
2026-07-19 19:56:31 +0900

    refactor: 수정된 D101 정책서 내용 반영

--- app/schemas/recommendation/recommendation.py
+++ app/schemas/recommendation/recommendation.py
@@ -6,17 +6,19 @@ from app.schemas.types import SourceType
 class RecommendationRequest(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
 
-    event_id: int = Field(alias= "eventId")
-    external_event_id : str | None = Field(default= None, alias= "externalEventId")
+    temp_event_id: str = Field(alias= "tempEventId")
+    draft_revision: int = Field(alias= "draftRevision")
+    event_title: str = Field(alias= "eventTitle", min_length=1)
     source_type: SourceType = Field(alias= "sourceType")
-    source_text: str = Field(alias= "sourceText", min_length=1)
-    start_date: date = Field(alias= "startDate")
-    start_time: time | None = Field(default= None, alias= "startTime")
-    end_date: date | None = Field(default= None, alias= "endDate")
-    end_time: time | None = Field(default= None, alias= "endTime")
-    location: str | None = None
+    start_date_candidate: date = Field(alias= "startDateCandidate")
+    start_time_candidate: time | None = Field(default= None, alias= "startTimeCandidate")
+    end_date_candidate: date | None = Field(default= None, alias= "endDateCandidate")
+    end_time_candidate: time | None = Field(default= None, alias= "endTimeCandidate")
+    place_candidate: str | None = Field(default= None, alias= "placeCandidate")
     description: str | None = None
+    external_event_id : str | None = Field(default= None, alias= "externalEventId")
     provider: str | None = None # external_event_id 값이 존재하면 provider 값도 존재해야함 해당 검증 로직은 service쪽에서 구현 예정
     embedding_words: list[str] = Field(alias="embeddingWords")
+
 # /api/v1/recommendations responseDTO
 
